154ea167cde57d8df326bf0a7ae5d6b45fb555f3
HanziQ <HanziQ42@gmail.com>
2012-09-12 09:27:18 +0200

    First code.

---
+++ Gateway/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Gateway
+{
+    public class Main
+    {
+        private ITaskList taskList;
+
+        public Main(ITaskList taskList)
+        {
+            this.taskList = taskList;
+            Run();
+        }
+
+        private void Run()
+        {
+            WriteHeader();
+            WritePrompt();
+            string read = Console.ReadLine();
+
+            Console.Clear();
+
+            int taskID;
+            if (int.TryParse(read, out taskID))
+            {
+                MethodInfo method = taskList.GetType().GetMethod("Task" + taskID.ToString());
+                if (method != null)
+                {
+                    method.Invoke(taskList, null);
+                }
+            }
+        }
+
+        string title = "Gateway";
+
+        private void WriteHeader()
+        {
+            Console.Write(new String('=', Console.WindowWidth));
+            int before = Console.WindowWidth / 2 - title.Length / 2;
+            Console.CursorLeft = before;
+            Console.WriteLine(title);
+            Console.CursorLeft = 0;
+            Console.WriteLine(new String('=', Console.WindowWidth));
+        }
+
+        private void WritePrompt()
+        {
+            Console.CursorTop = Console.WindowHeight - 3;
+            Console.WriteLine("Zadejte číslo úlohy:");
+            Console.Write(new String('-', Console.WindowWidth));
+            Console.Write('>');
+        }
+    }
+}
---
+++ TestApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gateway;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main gateway = new Main(new TaskList());
+
+        }
+    }
+}
