df5c4271983c32f2d5a07b911d2856108832ce31
HanziQ <HanziQ42@gmail.com>
2012-10-10 20:51:54 +0200

    Finished task 13.

--- TestApplication/Tasks/Task13.cs
+++ TestApplication/Tasks/Task13.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Gateway;
-using Gateway.Rules;
 
 namespace TestApplication.Tasks
 {
@@ -11,10 +8,20 @@ namespace TestApplication.Tasks
     {
         public void Process()
         {
-            int mesic = Input<int>.Get("Zadejte počet dní v měsíci.").AddRule(new Rule<int>((i) => {return i >= 28 & i <=31;}, "Zadejte platný počet dnů."));
+            int mesic = Input<int>.Get("Zadejte počet dní v měsíci.").AddRule(new Rule<int>((i) => { return i >= 28 & i <= 31; }, "Zadejte platný počet dnů."));
             int prvniDen = Input<int>.Get("Zadejte číslo prvního dne v měsíci.").AddRule(new Rule<int>((i) => { return i > 0 & i < 8; }, "Zadejte platné číslo."));
 
-            //int prvniPatek = (prvniDen < 6) ? 6 - prvniDen : 
+            int prvniPatek = (prvniDen < 6) ? 6 - prvniDen : 13 - prvniDen;
+            List<string> patky = new List<string>();
+            patky.Add(prvniPatek.ToString());
+            int den = prvniPatek;
+
+            while (den <= mesic - 7)
+            {
+                den += 7;
+                patky.Add(den.ToString());
+            }
+            Console.WriteLine("Počet pátků v tomto měsíci je: " + patky.Count + " - " + string.Join("., ", patky) + ".");
         }
     }
 }
