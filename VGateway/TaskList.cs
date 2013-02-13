23ceef0e69efc0d80d2987c0ae5d1863331acb92
HanziQ <HanziQ42@gmail.com>
2013-02-13 09:28:17 +0100

    VGateway start.

---
+++ VGateway/TaskList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace VGateway
+{
+    public class TaskList
+    {
+        public List<int> GetTasks()
+        {
+            List<int> nums = new List<int>();
+            MethodInfo[] mi = GetType().GetMethods();
+            foreach (MethodInfo m in mi)
+            {
+                Match ma = Regex.Match(m.Name, "Task(\\d+)");
+                if(ma.Success)
+                {
+                    nums.Add(Int32.Parse(ma.Groups[1].Value));
+                }
+            }
+            return nums;
+        }
+
+        public void Task1()
+        {
+
+        }
+
+        public void Task2()
+        {
+
+        }
+
+        public void Task3()
+        {
+
+        }
+    }
+}
---
+++ VGateway/TaskListForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VGateway
+{
+    public partial class TaskListForm : Form
+    {
+        TaskList taskList;
+
+        public TaskListForm(TaskList taskList)
+        {
+            this.taskList = taskList;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (int i in taskList.GetTasks())
+            {
+                Button b = new Button();
+                b.Text = i.ToString();
+                b.Click += (o, ea) => { DisplayTaskForm(i); };
+                flowLayoutPanel1.Controls.Add(b);
+            }
+        }
+
+        private void DisplayTaskForm(int i)
+        {
+            TaskForm tf = new TaskForm(taskList, i);
+            tf.FormClosed += (o, e) => { WindowState = FormWindowState.Normal; };
+            tf.Show();
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+}
